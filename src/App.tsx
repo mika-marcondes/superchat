09f1c39286b4b7b91989ae19165a31a81989066d
Mika <mika.marcondes2@gmail.com>
2022-09-19 20:29:58 -0300

    :construction: Display text on chat room

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,25 @@ function SignOut() {
 }
 
 function ChatRoom() {
-  return <SignOut />;
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+
+  const [messages] = useCollectionData(query, { idField: "id" });
+
+  return (
+    <>
+      <div>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+    </>
+  );
+}
+
+function ChatMessage(props) {
+  const { text } = props.message;
+
+  return <p>{text}</p>;
 }
 
 export default App;
